a057319f0bf9ebc65e2d8dfefbf55a46fefa7109
User <stparchkg@gmail.com>
2023-10-24 21:18:16 +0600

    add Exceptions add BuyerService

---
+++ product_shop/src/main/java/kg/java/product_shop/core/exceptions/EntityDuplicateExceptions.java
@@ -0,0 +1,7 @@
+package kg.java.product_shop.core.exceptions;
+
+public class EntityDuplicateExceptions extends Exception{
+    public EntityDuplicateExceptions() {
+        super("entity exist");
+    }
+}
