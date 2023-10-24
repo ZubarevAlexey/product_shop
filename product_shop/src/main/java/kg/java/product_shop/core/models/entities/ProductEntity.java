2b384055e963ba0edabdac00dc98aa0c85721f70
User <stparchkg@gmail.com>
2023-10-24 22:25:55 +0600

    add relations BuyerEntity, ProductEntity

--- product_shop/src/main/java/kg/java/product_shop/core/models/entities/ProductEntity.java
+++ product_shop/src/main/java/kg/java/product_shop/core/models/entities/ProductEntity.java
@@ -1,12 +1,17 @@
 package kg.java.product_shop.core.models.entities;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.*;
 import org.hibernate.annotations.CacheConcurrencyStrategy;
 
 import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.Set;
 
 @Entity
-@Data
+@Getter
+@Setter
 @NoArgsConstructor
 @AllArgsConstructor
 @Cacheable
@@ -20,4 +25,26 @@ public class ProductEntity {
     private Long id;
     private String name;
     private Long cost;
+    @OneToMany(mappedBy= "product")
+    @JsonIgnore
+    private Set<PurchaseEntity> purchases = new HashSet<>();
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ProductEntity product = (ProductEntity) o;
+        return Objects.equals(id, product.id) && Objects.equals(name, product.name) && Objects.equals(cost, product.cost) && Objects.equals(purchases, product.purchases);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, cost, purchases);
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
+
 }
